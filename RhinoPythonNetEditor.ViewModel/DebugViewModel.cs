71b1cf1b74b1b87495495a4df1ca1d8150701ee6
Torso <chikalee@outlook.com>
2022-09-24 23:31:11 +0800

    fix dir

--- RhinoPythonNetEditor.ViewModel/DebugViewModel.cs
+++ RhinoPythonNetEditor.ViewModel/DebugViewModel.cs
@@ -115,7 +115,7 @@ namespace RhinoPythonNetEditor.ViewModel
                 await fs.WriteAsync(bytes, 0, bytes.Length);
             }
             var file = $@"{dir}temp.py";
-            var script = $@"{PythonPath} -u -m debugpy --listen localhost:{port} --wait-for-client --log-to ~/logs ""{file}""";
+            var script = $@"{PythonPath} -u -m debugpy --listen localhost:{port} --wait-for-client --log-to {dir}logs ""{file}""";
             debugManager.DebugEnd += DebugManager_DebugEnd;
             debugManager.Stopped += DebugManager_Stopped;
             debugManager.ConfigDone += (s, e) => ConfigDone = true;
@@ -125,6 +125,7 @@ namespace RhinoPythonNetEditor.ViewModel
                 IsDebuging = true;
                 debugManager.Start(infos.Select(i=>i+LineOffset).ToList(), file);
             }
+           
         }
 
         private async void DebugManager_DebugEnd(object sender, EventArgs e)
